856d60bc6f86e6367e9f85601d3b6ca087fa32f4
Dmitriy Ovcharov <dovcarov@gmail.com>
2023-08-25 00:05:38 +0400

    updated capitalize.test.js and package-lock.json and package.json and added gitignore

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,8 @@
 import { capitalize } from '../src/capitalize.js';
+import { strict as assert } from 'node:assert';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+assert.deepEqual(capitalize('hello'), 'Hello', 'Функция работает неверно!')
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+assert.deepEqual(capitalize(''), '', 'Функция работает неверно!');
 
 console.log('Все тесты пройдены!');
